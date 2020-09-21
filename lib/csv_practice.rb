5c36fbd84f23ba7ad1de3159b2c3d2af1d92f6f9
beauttie <bak02013@pomona.edu>
2020-09-20 18:24:02 -0700

    Added get_all_olympic_athletes, total_medals_per_team, get_all_gold_medalists methods

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -2,12 +2,42 @@ require 'csv'
 require 'awesome_print'
 
 def get_all_olympic_athletes(filename)
+  dataset = CSV.read(filename, headers: true).map do |athlete|
+    {
+        "ID" => athlete["ID"],
+        "Name" => athlete["Name"],
+        "Height" => athlete["Height"],
+        "Team" => athlete["Team"],
+        "Year" => athlete["Year"],
+        "City" => athlete["City"],
+        "Sport" => athlete["Sport"],
+        "Event" => athlete["Event"],
+        "Medal" => athlete["Medal"]
+    }
+  end
 
+  return dataset
 end
 
 def total_medals_per_team(olympic_data)
+  medals_count = {}
 
+  olympic_data.each do |athlete|
+    if athlete["Medal"] != "NA"
+      # if team has not been counted yet
+      if medals_count[athlete["Team"]].nil?
+        medals_count[athlete["Team"]] = 1
+      else
+        medals_count[athlete["Team"]] += 1
+      end
+    end
+  end
+
+  return medals_count
 end
 
 def get_all_gold_medalists(olympic_data)
+  gold_medalists = olympic_data.filter { |athlete| athlete["Medal"] == "Gold" }
+
+  return gold_medalists
 end
